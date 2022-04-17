7e0c1ca0d3632d67f68d1e5fcff1045433d59c67
Kiran Revanna <87458505+KiranRevanna1@users.noreply.github.com>
2022-04-17 17:26:41 +0530

    first commit

--- Birthday_Wisher/settings.py
+++ Birthday_Wisher/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "your key"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','dsce-birthday-wisher.herokuapp.com']
 
 
 # Application definition
