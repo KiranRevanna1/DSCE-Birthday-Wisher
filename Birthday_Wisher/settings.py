69bd25060a59d364407041a923bbdbe35547b404
Kiran Revanna <87458505+KiranRevanna1@users.noreply.github.com>
2022-04-17 17:04:43 +0530

    first commit

--- Birthday_Wisher/settings.py
+++ Birthday_Wisher/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "your key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Birthday_Wisher/urls.py
+++ Birthday_Wisher/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from sender import execute
 from django.contrib import admin
 from django.urls import path
 from uploader.views import about, campus, home, dashboard, uploader, add_form
 from sender.views import sender, email_temp
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,4 @@ urlpatterns = [
     path('sender/', sender, name="sender"),
     path('wish/<str:sender>/<str:receiver>/', email_temp, name="email_temp")
 ]
+
